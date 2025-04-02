955df21189703ec81ef7ca8a38a135dd521b3343
Hyungeun Jo <geunee92@ship-da.com>
2025-04-02 17:02:27 +0900

    chore: 프로젝트 초기 셋팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { Global } from '@emotion/react'
+import globalStyles from './styles/globalStyles'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxFactory: '_jsx',
+    jsxFragment: '_jsxFragment',
+    jsxInject: `import { jsx as _jsx, jsxs as _jsxs, Fragment as _jsxFragment } from 'react/jsx-runtime'`,
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@shared': path.resolve(__dirname, 'src/components/shared'),
+    },
+  },
 })
